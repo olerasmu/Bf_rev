06aa2d6263be1dd24506afec0ba32ab1bda67ae5
olerasmu <ole.gilberg@gmail.com>
2014-04-10 17:18:21 +0200

    all working now

--- src/root/nested/implementation.py
+++ src/root/nested/implementation.py
@@ -5,6 +5,7 @@ Created on 25. mars 2014
 '''
 import math
 import os
+import timeit
 from Crypto.Cipher import AES 
 class Bf_reverse(object):
     
@@ -26,19 +27,19 @@ class Bf_reverse(object):
     
     #Slit the file into blocks of 128 bit (16*8 bytes)    
     def blockifyFile(self):
-        print "This is blckifyfile"
+        #print "This is blckifyfile"
         with open(self.filepath, 'rb') as newfile:
-            count = 0
+            #count = 0
             bytes = str(newfile.read(16))
             while bytes:
                 self.butterflyTab.append(bytes)
-                print bytes, " bytenr: ", count
-                count += 1 
+                #print bytes, " bytenr: ", count
+                #count += 1 
                 bytes = str(newfile.read(16))
             #print ("File has been divided into blocks")
             
     def blockifyFileTwo(self):
-        print "This is blckifyfile 2"
+        #print "This is blckifyfile 2"
         with open(self.filepath, 'rb') as newfile:
             for i in range(0, self.fileSize()/16):
                 byte = newfile.read(16)
@@ -72,13 +73,13 @@ class Bf_reverse(object):
     #Initialize the butterfly function. Variable j is controlled from here
     def initiateButterfly(self, d, n):
         self.fileTab = [None]*n
-        print (self.fileTab)
+        #print (self.fileTab)
         for j in range(d, 0, -1):
-            print ("Its j: ", j)
+            #print ("Its j: ", j)
             #print "this is j: ", j
             self.executeButterfly(j, n, d)
     
-    count = 0
+    #count = 0
     #Execute the butterfly algorithm
     def executeButterfly(self, j, n, d):
         #print ("execute")
@@ -90,14 +91,14 @@ class Bf_reverse(object):
                     indexOne = int(i+k*math.pow(2, j))-1
                     indexTwo = int(i+k*math.pow(2, j)+math.pow(2, j-1))-1
                     #print indexOne, " ", indexTwo
-                    self.count += 1
+                    #self.count += 1
                     self.w(self.butterflyTab[indexOne], self.butterflyTab[indexTwo], indexOne, indexTwo)
             else:   
                 for i in range(int(math.pow(2, j-1)), 0, -1):
                     indexOne = int(i+k*math.pow(2, j))-1
                     indexTwo = int(i+k*math.pow(2, j)+math.pow(2, j-1))-1
                     #print indexOne, " ", indexTwo, " from else you know"
-                    self.count += 1
+                    #self.count += 1
                     self.w(self.fileTab[indexOne], self.fileTab[indexTwo], indexOne, indexTwo)
                 #print ("this is i: ", i)
                 #print ("this is index one: ", indexOne, " and this is index two: ", indexTwo)
@@ -108,21 +109,21 @@ class Bf_reverse(object):
         #print ("count: ", self.count)
         #print (self.butterflyTab)      
     
+    cipher_machine = AES.new(b'This is a key123', AES.MODE_ECB)
     def w(self, blockOne, blockTwo, indexOne, indexTwo):
-        cipher_machine = AES.new(b'This is a key123', AES.MODE_ECB)
         #print blockOne, " ", blockTwo, " this is seperated"
         
-        blockOne2 = cipher_machine.decrypt(blockOne)
-        blockTwo2 = cipher_machine.decrypt(blockTwo)
+        blockOne2 = self.cipher_machine.decrypt(blockOne)
+        blockTwo2 = self.cipher_machine.decrypt(blockTwo)
         
         combostring = blockTwo2 + blockOne2
         
         new_block_one, new_block_two = combostring[0::2], combostring[1::2]
         
         
-        print len(combostring)
-        print combostring, "this is combostring"
-        print new_block_one, " ",  new_block_two, " this is after"
+        #print len(combostring)
+        #print combostring, "this is combostring"
+        #print new_block_one, " ",  new_block_two, " this is after"
         
         
         self.fileTab[indexOne] = new_block_one
@@ -131,20 +132,24 @@ class Bf_reverse(object):
         
         
 bf_rev = Bf_reverse(filepath = 'C:\\Users\\olerasmu\\Documents\\workspace\\Butterfly\\src\\root\\nested\\bf_hourglass.txt')
-print bf_rev.fileSize()
-print os.path.getsize(bf_rev.filepath)
+start  = timeit.default_timer()
+#print bf_rev.fileSize()
+#print os.path.getsize(bf_rev.filepath)
 bf_rev.blockifyFile()
 #bf_rev.blockifyFileTwo()
 #bf_rev.fileifyBlocks2()
-print "Butterflytab: ", bf_rev.butterflyTab
-print "filetab: ", bf_rev.fileTab
+#print "Butterflytab: ", bf_rev.butterflyTab
+#print "filetab: ", bf_rev.fileTab
 n = len(bf_rev.butterflyTab)
-print "This is n", n
+#print "This is n", n
 d = int(math.log(n, 2))
 
 bf_rev.initiateButterfly(d, n)
-print bf_rev.filepath
-print "This is filetab after: ", bf_rev.fileTab
-print len(bf_rev.fileTab)
+#print bf_rev.filepath
+#print "This is filetab after: ", bf_rev.fileTab
+#print len(bf_rev.fileTab)
 bf_rev.fileifyBlocks2()
 
+stop = timeit.default_timer()
+
+print "Start: ", start, "\n Stop: ", stop, "\n Time: ", stop - start
